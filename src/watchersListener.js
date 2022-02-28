39a8f38ebfb56eda56710b0b58c37f4052cca39c
ARTSOFT\csillaj <jenei_csilla@yahoo.com>
2022-02-28 17:46:33 +0200

    * BR-256: LiveCard always shows 0 viewers * BR-82:Drumeo app live chat users online fluctuates by 30+ users

--- src/watchersListener.js
+++ src/watchersListener.js
@@ -1,23 +1,19 @@
 import { StreamChat } from 'stream-chat';
-export default async (apiKey, chatId, userId, gsToken, callback) => {
+const numberOfStreamWatchers = async (apiKey, chatId, userId, gsToken) => {
   let client = StreamChat.getInstance(apiKey, {
-    timeout: 10000
+    timeout: 10000,
   });
-
-  let queryChat = async () =>
-    (
-      await client.queryChannels({ id: { $eq: chatId } }, [{}], {
-        watch: false,
-        message_limit: 0
-      })
-    )?.[0]?.state.watcher_count;
-
-  let listener = async ({ type }) =>
-    type === 'health.check' && callback(await queryChat());
-
   if (!client.user) await client.connectUser({ id: `${userId}` }, gsToken);
-  callback(await queryChat());
-  client?.on(listener);
 
-  return () => client?.off(listener);
+  let watcherCount = (
+    await client.queryChannels({ id: { $eq: chatId } }, [{}], {
+      watch: true,
+      message_limit: 0,
+    })
+  )?.[0]?.state.watcher_count;
+
+  await client?.disconnectUser?.();
+  return watcherCount;
 };
+
+export default numberOfStreamWatchers;
